d7edc6ce699a98f222c8b921951c056845a5a4ec
Louisa Smith <louisahsmith@gmail.com>
2023-09-24 13:58:57 -0400

    really trying to figure out where to put this

--- R/survey.R
+++ R/survey.R
@@ -71,8 +71,8 @@ aou_survey <- function(cohort,
     # gave one value for question_output and multiple values for questions
   } else {
     question_output_arg <- match.arg(question_output, c("text", "concept_id"))
-    question_output <- ifelse(question_output_arg == "concept_id", "concept_id", "value")
   }
+  question_output <- ifelse(length(question_output_arg) == 1 & question_output_arg == "concept_id", "concept_id", "value")
 
   answer_output <- match.arg(answer_output, c("text", "concept_id"))
   answer_output <- ifelse(answer_output == "text", "value", answer_output)
