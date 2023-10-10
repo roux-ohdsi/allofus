a06a7435244212244aa40592754a16cb5d465dbe
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2023-10-10 16:18:13 -0400

    Documenting

--- R/obs_period.R
+++ R/obs_period.R
@@ -47,18 +47,27 @@ aou_observation_period <- function(cohort,
       select(person_id, visit_start_date, visit_end_date) %>%
       distinct() %>%
       group_by(person_id) %>%
+    # use window order instead of arrange. because arrange == ORDER BY which is executed last in sql
       dbplyr::window_order(person_id, visit_start_date, visit_end_date) %>%
+    # get the last visit end date within each person
       mutate(last_end_date = lag(visit_end_date)) %>%
+    # calc dayes in bewteen visits. I'd like to just pass the bigquery DATE_DIFF() function
+    # instead of using CDMConnector but its not working well for me
       mutate(days_between_visits = !!CDMConnector::datediff("last_end_date", "visit_start_date", "day")) %>%
+    # iterate over the observation period if days between visits is more than the persistence window
       mutate(obs_period = cumsum(ifelse(days_between_visits > persistence_window, 1, 0))) %>%
+    # the first observation per person is NA , but can just be changed to 1
       mutate(obs_period = ifelse(is.na(obs_period), 1, obs_period+1)) %>%
+    # get the minimum and maximums leftover
       group_by(person_id, obs_period) %>%
       summarize(observation_start_date = min(visit_start_date),
                 observation_end_date = max(visit_end_date), .groups = 'drop') %>%
       group_by(person_id) %>%
+    # pad the end date
       mutate(observation_end_date = !!CDMConnector::dateadd("observation_end_date", end_date_buffer, "day")) %>%
       arrange(person_id, obs_period)
 
+  # collect if desired.
   if(isTRUE(collect)){
     obs_period = collect(obs_period)
   }
