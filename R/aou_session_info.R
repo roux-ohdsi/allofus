9524d314757d43561034fb20138a2639852e76e4
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2023-11-07 15:51:18 -0500

    aou_session_ingo

---
+++ R/aou_session_info.R
@@ -0,0 +1,22 @@
+#' Print session information for the AoU R environment
+#'
+#' @param CDR The name of the CDR to use. Defaults to `getOption("aou.default.cdr")`
+#'
+#' @return A list with three elements: the platform, the AoU release, and the packages
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   con <- allofus::aou_connect()
+#'   allofus::aou_session_info()
+#' }
+aou_session_info <- function(CDR = getOption("aou.default.cdr")){
+  si = sessioninfo::session_info()
+  si1 = t$platform
+  si2 = aou_sql(query = "SELECT * FROM {CDR}._cdr_metadata")
+  si3 = t$packages
+
+  out = list(si1, si2, si3)
+  names(out) = c("Platform", "AoU_Release", "Packages")
+  return(out)
+}
