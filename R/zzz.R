16dc0565540f8e1ee24f9147e7d41dbc4fd6a002
rbcavanaugh <r.cavanaugh@northeastern.edu>
2023-12-08 15:18:24 -0500

    startup note

--- R/zzz.R
+++ R/zzz.R
@@ -14,9 +14,19 @@
 
 }
 
+greet_startup <- function() {
+  msg <- paste0(
+    c(
+      "{cli::symbol$heart} Thank you for using the {.pkg allofus} R package! {cli::symbol$heart}",
+      "{cli::symbol$warning} This package is currently in beta. Please report any issues to {.url https://github.com/roux-ohdsi/allofus/issues}.",
+      "{cli::symbol$info} The {.pkg allofus} R package is not affiliated with or endorsed by the AllofUs Research Program. \n\n"
+      ),
+    collapse = "\n"
+  )
+  rlang::inform(cli::format_inline(msg), class = "packageStartupMessage")
+}
+
 .onAttach <- function(libname = find.package("allofus"), pkgname = "allofus"){
-    cli::cli_inform(c("i" = "Thank you for using the {.pkg allofus} R package!",
-                    "This package is currently in beta. Please report any issues to {.url https://github.com/roux-ohdsi/allofus/issues}.",
-                    "The {.pkg allofus} R package is not affiliated with or endorsed by the AllofUs Research Program. \n\n"))
+  greet_startup()
 }
 
